dc1afd137a64524cf3f3ccd4f1dd0ae99cacd57c
Gabriel Quinteros <Gax_95@hotmail.com>
2024-03-20 21:29:10 -0300

    Parte Visual de Pantalla de Marcas

--- src/main/java/Producto.java
+++
@@ -1,7 +0,0 @@
-
-public class Producto {
-
-	private int id;
-	
-	
-}
--- src/main/java/pantallas/pantallasABM/marcas/PlantillaPantallas.java
+++ src/main/java/pantallas/pantallasABM/marcas/PlantillaPantallas.java
@@ -1,8 +1,9 @@
-package pantallas.pantallas;
+package pantallas.pantallasABM.marcas;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
 import com.formdev.flatlaf.FlatLightLaf;
@@ -22,6 +23,7 @@ import java.awt.SystemColor;
 import java.awt.Component;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
@@ -39,11 +41,26 @@ import java.awt.event.ActionEvent;
 import java.awt.Cursor;
 import javax.swing.border.CompoundBorder;
 import javax.swing.ImageIcon;
+import java.awt.FlowLayout;
+
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.ListSelectionModel;
+import javax.swing.JTextField;
+import javax.swing.JEditorPane;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import java.awt.TextArea;
+import java.awt.TextField;
 
 public class PlantillaPantallas extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel panelGeneral;
+	private JTable table;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -79,10 +96,13 @@ public class PlantillaPantallas extends JFrame {
 		    }
 		   
 		});
-		UIManager.put( "Button.arc", 100 );
-		UIManager.put( "Component.arc", 100 );
-		UIManager.put( "ProgressBar.arc", 100 );
-		UIManager.put( "TextComponent.arc", 100 );
+		//UIManager.put( "Button.arc", 100 );
+		//UIManager.put( "Component.arc", 100 );
+		//UIManager.put( "ProgressBar.arc", 100 );
+		//UIManager.put( "TextComponent.arc", 100 );
+		
+		
+		
 		
 		
 		
@@ -230,7 +250,7 @@ public class PlantillaPantallas extends JFrame {
 		panelGeneral.add(panelTituloVentana);
 		panelTituloVentana.setLayout(null);
 		
-		JLabel titleFunc = new JLabel("FUNCIONALIDAD");
+		JLabel titleFunc = new JLabel("REGISTRAR MARCA");
 		titleFunc.setForeground(SystemColor.text);
 		titleFunc.setBounds(10, 10, 561, 40);
 		panelTituloVentana.add(titleFunc);
@@ -239,8 +259,18 @@ public class PlantillaPantallas extends JFrame {
 		
 		
 		JPanel panelPaddingBot = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panelPaddingBot.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
 		panelPaddingBot.setBounds(178, 87, 581, 27);
 		panelGeneral.add(panelPaddingBot);
+		
+		JLabel lblNewLabel = new JLabel("Marcas registradas:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblNewLabel.putClientProperty( "FlatLaf.styleClass", "h3" );
+		panelPaddingBot.add(lblNewLabel);
+		
+		
 		
 		JPanel panelPaddingTop = new JPanel();
 		panelPaddingTop.setBounds(178, 0, 581, 27);
@@ -259,5 +289,130 @@ public class PlantillaPantallas extends JFrame {
 		JPanel panelUtilidad = new JPanel();
 		panelUtilidad.setBounds(178, 112, 581, 364);
 		panelGeneral.add(panelUtilidad);
+		panelUtilidad.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 10, 561, 174);
+		panelUtilidad.add(panel);
+		panel.setLayout(null);
+		
+		
+		/// TABLA/////////////////////////////////////////////////  //////////////
+		// COLUMNAS
+		String[] columnNames = {"Marca", "Descripción"};
+        // FILAS
+        Object[][] data = {
+            {"8 BLOQ", "Descripción A"},
+            {"Andif", "Descripción B"},
+            {"Travex", "Descripción C"},
+            {"MAC", "Descripción D"},
+            {"PRIVE", "Descripción A"},
+            {"DAC", "Descripción B"},
+            {"ANUAC", "Descripción C"},
+            {"YALE", "Descripción D"}
+        };
+        
+     // Crear el modelo de la tabla y la tabla
+        DefaultTableModel model = new DefaultTableModel(data, columnNames);
+        JTable table = new JTable(model);
+		table.setBounds(0, 0, 561, 155); 
+		// Establecer la selección por filas
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        
+        // Hacer que la tabla no sea editable
+        table.setDefaultEditor(Object.class, null);
+        
+        // HACER QUE EL USUARIO NO PUEDA CAMBIAR DE TAMAÑO LAS COLUMNAS
+        table.getTableHeader().setResizingAllowed(false);
+        
+        // HACER QUE EL USUARIO NO PUEDA CAMBIAR EL ORDEN DE LAS COMUNAS
+        table.getTableHeader().setReorderingAllowed(false);
+        
+        DefaultTableCellRenderer renderer = new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+                    boolean hasFocus, int row, int column) {
+                Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                if (isSelected) {
+                    c.setBackground(table.getSelectionBackground());
+                    c.setForeground(table.getSelectionForeground());
+                    setBorder(null); // Quitar el borde de selección
+                } else {
+                    c.setBackground(table.getBackground());
+                    c.setForeground(table.getForeground());
+                }
+                return c;
+            }
+        };
+        
+        
+        
+        
+        // Añadir la tabla a un JScrollPane
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setBounds(0, 10, 561, 152);
+		panel.add(scrollPane);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(10, 206, 213, 44);
+		panelUtilidad.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel_4 = new JLabel("Marca");
+		lblNewLabel_4.setBounds(0, 0, 45, 13);
+		lblNewLabel_4.putClientProperty( "FlatLaf.styleClass", "h4" );
+		panel_1.add(lblNewLabel_4);
+		
+		
+		
+		textField = new JTextField();
+		textField.setBounds(0, 15, 193, 29);
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		JPanel panel_1_1 = new JPanel();
+		panel_1_1.setLayout(null);
+		panel_1_1.setBounds(10, 261, 213, 44);
+		panelUtilidad.add(panel_1_1);
+		
+		JLabel lblNewLabel_4_1 = new JLabel("Descripción");
+		lblNewLabel_4_1.setBounds(0, 0, 83, 13);
+		lblNewLabel_4_1.putClientProperty( "FlatLaf.styleClass", "h4" );
+		panel_1_1.add(lblNewLabel_4_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(0, 15, 193, 29);
+		panel_1_1.add(textField_1);
+		
+		JButton btnNewButton_2 = new JButton("Registrar");
+		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 10));
+		btnNewButton_2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewButton_2.setBounds(10, 320, 85, 25);
+		panelUtilidad.add(btnNewButton_2);
+		
+		JButton btnNewButton_2_1 = new JButton("Modificar");
+		btnNewButton_2_1.setFont(new Font("Tahoma", Font.BOLD, 10));
+		btnNewButton_2_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewButton_2_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2_1.setBounds(115, 320, 85, 25);
+		panelUtilidad.add(btnNewButton_2_1);
+		
+		JButton btnNewButton_2_2 = new JButton("Eliminar");
+		btnNewButton_2_2.setFont(new Font("Tahoma", Font.BOLD, 10));
+		btnNewButton_2_2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewButton_2_2.setBounds(220, 320, 85, 25);
+		panelUtilidad.add(btnNewButton_2_2);
+		
+		JLabel imagMarcas = new JLabel("");
+		imagMarcas.setIcon(new ImageIcon("C:\\Users\\Gabriel Quinteros\\Desktop\\GitHub\\Proyectos para Portfolio\\Cerrajeria-Java-MySQL-Hibernate\\src\\main\\resources\\imagenes\\Marcas 2.png"));
+		imagMarcas.setBounds(343, 206, 228, 88);
+		panelUtilidad.add(imagMarcas);
+		
+		
+		
 	}
 }
